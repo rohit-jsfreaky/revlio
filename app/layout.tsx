ecb489a7ff348a263471713064a5a4646fa549b1
rohit-jsfreaky <rohitkashyapmrt@gmail.com>
2026-02-02 11:17:44 +0530

    fix(favicon) favicon not showing on google fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -114,8 +114,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/revlio_logo.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/revlio_logo.png" type="image/png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/revlio_logo.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
